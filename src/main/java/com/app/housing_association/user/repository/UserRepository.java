9204478b95a0a064ffe365cdc3c344aa9d936867
Sebastian Tkaczyk <sebastian.tkaczykk@gmail.com>
2022-04-15 20:40:58 +0200

    feat: Add UserRepository in user package

---
+++ src/main/java/com/app/housing_association/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.app.housing_association.user.repository;
+
+import com.app.housing_association.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}

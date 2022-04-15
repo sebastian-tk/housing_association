b267f7edc810d035d13a8f86f5a5b34e1b1b6500
Sebastian Tkaczyk <sebastian.tkaczykk@gmail.com>
2022-04-15 16:55:20 +0200

    commit init

---
+++ src/main/java/com/app/housing_association/HousingAssociationApplication.java
@@ -0,0 +1,13 @@
+package com.app.housing_association;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingAssociationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousingAssociationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/housing_association/HousingAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.housing_association;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingAssociationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

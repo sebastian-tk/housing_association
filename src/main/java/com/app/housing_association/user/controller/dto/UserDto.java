51e478690c7895634444f731303c5b787ea481c0
Sebastian Tkaczyk <sebastian.tkaczykk@gmail.com>
2022-04-15 20:47:54 +0200

    feat: Add UserDto to dto package in controller of user

---
+++ src/main/java/com/app/housing_association/user/controller/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.app.housing_association.user.controller.dto;
+
+import com.app.housing_association.common.model.BaseDto;
+import com.app.housing_association.user.entity.enums.Role;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto extends BaseDto<Long> {
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
+}

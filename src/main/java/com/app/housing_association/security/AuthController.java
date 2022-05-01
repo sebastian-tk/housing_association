96cc0d5c1347512867ec6569958dc3b7ad41c5f3
Sebastian Tkaczyk <sebastian.tkaczykk@gmail.com>
2022-05-01 18:24:56 +0200

    feat: Add AuthController as login and registration controller

--- src/main/java/com/app/housing_association/security/AuthController.java
+++ src/main/java/com/app/housing_association/security/AuthController.java
@@ -3,7 +3,8 @@ package com.app.housing_association.security;
 import com.app.housing_association.security.model.LoggedUser;
 import com.app.housing_association.security.model.LoginCredentials;
 import com.app.housing_association.security.model.AdminRegistrationData;
-import com.app.housing_association.security.registration.service.UserAdminService;
+import com.app.housing_association.security.service.LoginService;
+import com.app.housing_association.security.service.RegistrationService;
 import com.app.housing_association.user.controller.dto.UserDto;
 import com.app.housing_association.user.controller.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
@@ -20,22 +21,22 @@ import java.util.Optional;
 @RestController
 public class AuthController {
 
-    private final AuthService authService;
+    private final LoginService loginService;
 
-    private final UserAdminService userAdminService;
+    private final RegistrationService registrationService;
 
     private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<LoggedUser> authenticateUser(@Valid @RequestBody LoginCredentials loginCredentials) {
-        Authentication authentication = authService.checkLoginCredentials(loginCredentials);
-        return ResponseEntity.ok(authService.getLoggedUser(authentication));
+        Authentication authentication = loginService.checkLoginCredentials(loginCredentials);
+        return ResponseEntity.ok(loginService.getLoggedUser(authentication));
     }
 
     @PostMapping("/registration")
     public ResponseEntity<UserDto> registration(@RequestBody AdminRegistrationData adminRegistrationData) {
         return Optional.of(adminRegistrationData)
-                .flatMap(userAdminService::registration)
+                .flatMap(registrationService::registration)
                 .map(userMapper::toRegisteredDto)
                 .map(dto -> ResponseEntity.ok().body(dto))
                 .orElseThrow();

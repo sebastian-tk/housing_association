43386c76efa8692303624d293c4c497ec686f7de
Sebastian Tkaczyk <sebastian.tkaczykk@gmail.com>
2022-07-28 21:11:59 +0200

    feat: Add new fields in User with example data

---
+++ src/main/resources/db/migration/V8__add_users_fields.sql
@@ -0,0 +1,137 @@
+ALTER TABLE users ADD firstname VARCHAR(60);
+ALTER TABLE users ADD lastname VARCHAR(60);
+ALTER TABLE users ADD phone_number VARCHAR(11);
+
+UPDATE users SET firstname = 'Admin', lastname= 'Admin', phone_number='000-000-000',username='admin' WHERE id = 1;
+
+UPDATE users SET firstname = 'Barbara', lastname= 'Wojcik', phone_number='600-500-900',username='barbara1' WHERE id = 2;
+UPDATE users SET firstname = 'Adam', lastname= 'Wojcik', phone_number='600-500-902',username='adam1' WHERE id = 3;
+UPDATE users SET firstname = 'Cecylia', lastname= 'Wojcik', phone_number='600-500-903',username='cecylia1' WHERE id =4 ;
+UPDATE users SET firstname = 'Daniel', lastname= 'Wojcik', phone_number='600-500-904',username='daniel1' WHERE id = 5;
+UPDATE users SET firstname = 'Eryk', lastname= 'Wojcik', phone_number='600-500-905',username='eryk1' WHERE id = 6;
+UPDATE users SET firstname = 'Albert', lastname= 'Wojcik', phone_number='600-500-906',username='albert1' WHERE id = 7;
+UPDATE users SET firstname = 'Pawel', lastname= 'Wojcik', phone_number='600-500-907',username='pawel1' WHERE id = 8;
+UPDATE users SET firstname = 'Iza', lastname= 'Wojcik', phone_number='600-500-908',username='iza1' WHERE id = 9;
+UPDATE users SET firstname = 'Monika', lastname= 'Wojcik', phone_number='600-500-909',username='monika1' WHERE id = 10;
+UPDATE users SET firstname = 'Anna', lastname= 'Wojcik', phone_number='600-500-919',username='anna1' WHERE id = 11;
+
+UPDATE users SET firstname = 'Barbara', lastname= 'Kowalski', phone_number='600-500-920',username='barbara2' WHERE id = 12;
+UPDATE users SET firstname = 'Adam', lastname= 'Kowalski', phone_number='600-500-922',username='adam2' WHERE id = 13;
+UPDATE users SET firstname = 'Cecylia', lastname= 'Kowalski', phone_number='600-500-923',username='cecylia2' WHERE id =14 ;
+UPDATE users SET firstname = 'Daniel', lastname= 'Kowalski', phone_number='600-500-924',username='daniel2' WHERE id = 15;
+UPDATE users SET firstname = 'Eryk', lastname= 'Kowalski', phone_number='600-500-925',username='eryk2' WHERE id = 16;
+UPDATE users SET firstname = 'Albert', lastname= 'Kowalski', phone_number='600-500-926',username='albert2' WHERE id = 17;
+UPDATE users SET firstname = 'Pawel', lastname= 'Kowalski', phone_number='600-500-927',username='pawel2' WHERE id = 18;
+UPDATE users SET firstname = 'Iza', lastname= 'Kowalski', phone_number='600-500-928',username='iza2' WHERE id = 19;
+UPDATE users SET firstname = 'Monika', lastname= 'Kowalski', phone_number='600-500-929',username='monika2' WHERE id = 20;
+UPDATE users SET firstname = 'Anna', lastname= 'Kowalski', phone_number='600-500-929',username='anna2' WHERE id = 21;
+
+UPDATE users SET firstname = 'Barbara', lastname= 'Nowak', phone_number='600-500-800',username='barbaraN' WHERE id = 22;
+UPDATE users SET firstname = 'Adam', lastname= 'Nowak', phone_number='600-500-802',username='adamN' WHERE id = 23;
+UPDATE users SET firstname = 'Cecylia', lastname= 'Nowak', phone_number='600-500-803',username='cecyliaN' WHERE id = 24 ;
+UPDATE users SET firstname = 'Daniel', lastname= 'Nowak', phone_number='600-500-804',username='danielN' WHERE id = 25;
+UPDATE users SET firstname = 'Eryk', lastname= 'Nowak', phone_number='600-500-805',username='erykN' WHERE id = 26;
+UPDATE users SET firstname = 'Albert', lastname= 'Nowak', phone_number='600-500-806',username='albertN' WHERE id = 27;
+UPDATE users SET firstname = 'Pawel', lastname= 'Nowak', phone_number='600-500-807',username='pawelN' WHERE id = 28;
+UPDATE users SET firstname = 'Iza', lastname= 'Nowak', phone_number='600-500-808',username='izaN' WHERE id = 29;
+UPDATE users SET firstname = 'Monika', lastname= 'Nowak', phone_number='600-500-809',username='monikaN' WHERE id = 30;
+UPDATE users SET firstname = 'Anna', lastname= 'Nowak', phone_number='600-500-819',username='annaN' WHERE id = 31;
+
+UPDATE users SET firstname = 'Barbara', lastname= 'Hajto', phone_number='600-500-700',username='barbaraH' WHERE id = 32;
+UPDATE users SET firstname = 'Adam', lastname= 'Hajto', phone_number='600-500-702',username='adamH' WHERE id = 33;
+UPDATE users SET firstname = 'Cecylia', lastname= 'Hajto', phone_number='600-500-703',username='cecyliaH' WHERE id = 34 ;
+UPDATE users SET firstname = 'Daniel', lastname= 'Hajto', phone_number='600-500-704',username='danielH' WHERE id = 35;
+UPDATE users SET firstname = 'Eryk', lastname= 'Hajto', phone_number='600-500-705',username='erykH' WHERE id = 36;
+UPDATE users SET firstname = 'Albert', lastname= 'Hajto', phone_number='600-500-706',username='albertH' WHERE id = 37;
+UPDATE users SET firstname = 'Pawel', lastname= 'Hajto', phone_number='600-500-707',username='pawelH' WHERE id = 38;
+UPDATE users SET firstname = 'Iza', lastname= 'Hajto', phone_number='600-500-708',username='izaH' WHERE id = 39;
+UPDATE users SET firstname = 'Monika', lastname= 'Hajto', phone_number='600-500-709',username='monikaH' WHERE id = 40;
+UPDATE users SET firstname = 'Anna', lastname= 'Hajto', phone_number='600-500-719',username='annaH' WHERE id = 41;
+
+UPDATE users SET firstname = 'Barbara', lastname= 'Stoch', phone_number='600-500-600',username='barbaraSt' WHERE id = 42;
+UPDATE users SET firstname = 'Adam', lastname= 'Stoch', phone_number='600-500-602',username='adamSt' WHERE id = 43;
+UPDATE users SET firstname = 'Cecylia', lastname= 'Stoch', phone_number='600-500-603',username='cecyliaSt' WHERE id = 44 ;
+UPDATE users SET firstname = 'Daniel', lastname= 'Stoch', phone_number='600-500-604',username='danielSt' WHERE id = 45;
+UPDATE users SET firstname = 'Eryk', lastname= 'Stoch', phone_number='600-500-605',username='erykSt' WHERE id = 46;
+UPDATE users SET firstname = 'Albert', lastname= 'Stoch', phone_number='600-500-606',username='albertSt' WHERE id = 47;
+UPDATE users SET firstname = 'Pawel', lastname= 'Stoch', phone_number='600-500-607',username='pawelSt' WHERE id = 48;
+UPDATE users SET firstname = 'Iza', lastname= 'Stoch', phone_number='600-500-608',username='izaSt' WHERE id = 49;
+UPDATE users SET firstname = 'Monika', lastname= 'Stoch', phone_number='600-500-609',username='monikaSt' WHERE id = 50;
+UPDATE users SET firstname = 'Anna', lastname= 'Stoch', phone_number='600-500-619',username='annaSt' WHERE id = 51;
+
+UPDATE users SET firstname = 'Barbara', lastname= 'Blachowicz', phone_number='600-500-500',username='barbaraB' WHERE id = 52;
+UPDATE users SET firstname = 'Adam', lastname= 'Blachowicz', phone_number='600-500-502',username='adamB' WHERE id = 53;
+UPDATE users SET firstname = 'Cecylia', lastname= 'Blachowicz', phone_number='600-500-503',username='cecyliaB' WHERE id = 54 ;
+UPDATE users SET firstname = 'Daniel', lastname= 'Blachowicz', phone_number='600-500-504',username='danielB' WHERE id = 55;
+UPDATE users SET firstname = 'Eryk', lastname= 'Blachowicz', phone_number='600-500-505',username='erykB' WHERE id = 56;
+UPDATE users SET firstname = 'Albert', lastname= 'Blachowicz', phone_number='600-500-506',username='albertB' WHERE id = 57;
+UPDATE users SET firstname = 'Pawel', lastname= 'Blachowicz', phone_number='600-500-507',username='pawelB' WHERE id = 58;
+UPDATE users SET firstname = 'Iza', lastname= 'Blachowicz', phone_number='600-500-508',username='izaB' WHERE id = 59;
+UPDATE users SET firstname = 'Monika', lastname= 'Blachowicz', phone_number='600-500-509',username='monikaB' WHERE id = 60;
+UPDATE users SET firstname = 'Anna', lastname= 'Blachowicz', phone_number='600-500-519',username='annaB' WHERE id = 61;
+
+UPDATE users SET firstname = 'Barbara', lastname= 'Glik', phone_number='600-500-400',username='barbaraG' WHERE id = 62;
+UPDATE users SET firstname = 'Adam', lastname= 'Glik', phone_number='600-500-402',username='adamG' WHERE id = 63;
+UPDATE users SET firstname = 'Cecylia', lastname= 'Glik', phone_number='600-500-403',username='cecyliaG' WHERE id = 64 ;
+UPDATE users SET firstname = 'Daniel', lastname= 'Glik', phone_number='600-500-404',username='danielG' WHERE id = 65;
+UPDATE users SET firstname = 'Eryk', lastname= 'Glik', phone_number='600-500-405',username='erykG' WHERE id = 66;
+UPDATE users SET firstname = 'Albert', lastname= 'Glik', phone_number='600-500-406',username='albertG' WHERE id = 67;
+UPDATE users SET firstname = 'Pawel', lastname= 'Glik', phone_number='600-500-407',username='pawelG' WHERE id = 68;
+UPDATE users SET firstname = 'Iza', lastname= 'Glik', phone_number='600-500-408',username='izaG' WHERE id = 69;
+UPDATE users SET firstname = 'Monika', lastname= 'Glik', phone_number='600-500-409',username='monikaG' WHERE id = 70;
+UPDATE users SET firstname = 'Anna', lastname= 'Glik', phone_number='600-500-419',username='annaG' WHERE id = 71;
+
+UPDATE users SET firstname = 'Barbara', lastname= 'Kowalczyk', phone_number='600-500-300',username='barbaraK' WHERE id = 72;
+UPDATE users SET firstname = 'Adam', lastname= 'Kowalczyk', phone_number='600-500-302',username='adamK' WHERE id = 73;
+UPDATE users SET firstname = 'Cecylia', lastname= 'Kowalczyk', phone_number='600-500-303',username='cecyliaK' WHERE id = 74 ;
+UPDATE users SET firstname = 'Daniel', lastname= 'Kowalczyk', phone_number='600-500-304',username='danielK' WHERE id = 75;
+UPDATE users SET firstname = 'Eryk', lastname= 'Kowalczyk', phone_number='600-500-305',username='erykK' WHERE id = 76;
+UPDATE users SET firstname = 'Albert', lastname= 'Kowalczyk', phone_number='600-500-306',username='albertK' WHERE id = 77;
+UPDATE users SET firstname = 'Pawel', lastname= 'Kowalczyk', phone_number='600-500-307',username='pawelK' WHERE id = 78;
+UPDATE users SET firstname = 'Iza', lastname= 'Kowalczyk', phone_number='600-500-308',username='izaK' WHERE id = 79;
+UPDATE users SET firstname = 'Monika', lastname= 'Kowalczyk', phone_number='600-500-309',username='monikaK' WHERE id = 80;
+UPDATE users SET firstname = 'Anna', lastname= 'Kowalczyk', phone_number='600-500-319',username='annaK' WHERE id = 81;
+
+UPDATE users SET firstname = 'Barbara', lastname= 'Mazur', phone_number='600-500-200',username='barbaraM' WHERE id = 82;
+UPDATE users SET firstname = 'Adam', lastname= 'Mazur', phone_number='600-500-202',username='adamM' WHERE id = 83;
+UPDATE users SET firstname = 'Cecylia', lastname= 'Mazur', phone_number='600-500-203',username='cecyliaM' WHERE id = 84 ;
+UPDATE users SET firstname = 'Daniel', lastname= 'Mazur', phone_number='600-500-204',username='danielM' WHERE id = 85;
+UPDATE users SET firstname = 'Eryk', lastname= 'Mazur', phone_number='600-500-205',username='erykM' WHERE id = 86;
+UPDATE users SET firstname = 'Albert', lastname= 'Mazur', phone_number='600-500-206',username='albertM' WHERE id = 87;
+UPDATE users SET firstname = 'Pawel', lastname= 'Mazur', phone_number='600-500-207',username='pawelM' WHERE id = 88;
+UPDATE users SET firstname = 'Iza', lastname= 'Mazur', phone_number='600-500-208',username='izaM' WHERE id = 89;
+UPDATE users SET firstname = 'Monika', lastname= 'Mazur', phone_number='600-500-209',username='monikaM' WHERE id = 90;
+UPDATE users SET firstname = 'Anna', lastname= 'Mazur', phone_number='600-500-219',username='annaM' WHERE id = 91;
+
+UPDATE users SET firstname = 'Barbara', lastname= 'Krawczyk', phone_number='600-500-100',username='barbaraKr' WHERE id = 92;
+UPDATE users SET firstname = 'Adam', lastname= 'Krawczyk', phone_number='600-500-102',username='adamKr' WHERE id = 93;
+UPDATE users SET firstname = 'Cecylia', lastname= 'Krawczyk', phone_number='600-500-103',username='cecyliaKr' WHERE id = 94 ;
+UPDATE users SET firstname = 'Daniel', lastname= 'Krawczyk', phone_number='600-500-104',username='danielKr' WHERE id = 95;
+UPDATE users SET firstname = 'Eryk', lastname= 'Krawczyk', phone_number='600-500-105',username='erykKr' WHERE id = 96;
+UPDATE users SET firstname = 'Albert', lastname= 'Krawczyk', phone_number='600-500-106',username='albertKr' WHERE id = 97;
+UPDATE users SET firstname = 'Pawel', lastname= 'Krawczyk', phone_number='600-500-107',username='pawelKr' WHERE id = 98;
+UPDATE users SET firstname = 'Iza', lastname= 'Krawczyk', phone_number='600-500-108',username='izaKr' WHERE id = 99;
+UPDATE users SET firstname = 'Monika', lastname= 'Krawczyk', phone_number='600-500-109',username='monikaKr' WHERE id = 100;
+UPDATE users SET firstname = 'Anna', lastname= 'Krawczyk', phone_number='600-500-119',username='annaKr' WHERE id = 101;
+
+UPDATE users SET firstname = 'Barbara', lastname= 'Polak', phone_number='600-501-100',username='barbaraP' WHERE id = 102;
+UPDATE users SET firstname = 'Adam', lastname= 'Polak', phone_number='600-502-102',username='adamP' WHERE id = 103;
+UPDATE users SET firstname = 'Cecylia', lastname= 'Polak', phone_number='600-503-103',username='cecyliaP' WHERE id = 104 ;
+UPDATE users SET firstname = 'Daniel', lastname= 'Polak', phone_number='600-504-104',username='danielP' WHERE id = 105;
+UPDATE users SET firstname = 'Eryk', lastname= 'Polak', phone_number='600-500-105',username='erykP' WHERE id = 106;
+UPDATE users SET firstname = 'Albert', lastname= 'Polak', phone_number='600-505-106',username='albertP' WHERE id = 107;
+UPDATE users SET firstname = 'Pawel', lastname= 'Polak', phone_number='600-506-107',username='pawelP' WHERE id = 108;
+UPDATE users SET firstname = 'Iza', lastname= 'Polak', phone_number='600-507-108',username='izaP' WHERE id = 109;
+UPDATE users SET firstname = 'Monika', lastname= 'Polak', phone_number='600-508-109',username='monikaP' WHERE id = 110;
+UPDATE users SET firstname = 'Anna', lastname= 'Polak', phone_number='600-509-119',username='annaP' WHERE id = 111;
+
+UPDATE users SET firstname = 'Barbara', lastname= 'Szewczyk', phone_number='600-401-100',username='barbaraS' WHERE id = 112;
+UPDATE users SET firstname = 'Adam', lastname= 'Szewczyk', phone_number='600-402-102',username='adamS' WHERE id = 113;
+UPDATE users SET firstname = 'Cecylia', lastname= 'Szewczyk', phone_number='600-403-103',username='cecyliaS' WHERE id = 114 ;
+UPDATE users SET firstname = 'Daniel', lastname= 'Szewczyk', phone_number='600-404-104',username='danielS' WHERE id = 115;
+UPDATE users SET firstname = 'Eryk', lastname= 'Szewczyk', phone_number='600-400-105',username='erykS' WHERE id = 116;
+UPDATE users SET firstname = 'Albert', lastname= 'Szewczyk', phone_number='600-405-106',username='albertS' WHERE id = 117;
+UPDATE users SET firstname = 'Pawel', lastname= 'Szewczyk', phone_number='600-406-107',username='pawelS' WHERE id = 118;
+UPDATE users SET firstname = 'Iza', lastname= 'Szewczyk', phone_number='600-407-108',username='izaS' WHERE id = 119;
+UPDATE users SET firstname = 'Monika', lastname= 'Szewczyk', phone_number='600-408-109',username='monikaS' WHERE id = 120;
+UPDATE users SET firstname = 'Anna', lastname= 'Szewczyk', phone_number='600-409-119',username='annaS' WHERE id = 121;
